4e9bbe08feb132f7c5f36fec1f54e2fae418ea25
Gnong <jinhongdev@gmail.com>
2022-04-08 18:33:58 +0900

    Update discord message data

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const discord = new Webhook(
 const options = {
   headers: { "x-allthatnode-api-key": "ejJU9NP9KSmk0yDhS7CJhnedZ0G9gBwz" },
 };
-let blockHeight = 7141412;
+let blockHeight;
+const latestBlock = async () => {
+  await axios
+    .get(
+      `https://terra-mainnet-rpc.allthatnode.com:1317/blocks/latest`,
+      "",
+      options
+    )
+    .then(async (res) => {
+      blockHeight = res.data.block.header.height;
+    });
+};
+
 const checkBlockInfo = async () => {
   await axios
     .get(
@@ -18,19 +30,24 @@ const checkBlockInfo = async () => {
       if (res.data.status) {
         return;
       }
-      // if (res.data.block.last_commit.signatures[4].validator_address === "") {
-      msg = `Block Proposer\n ${res.data.block.header.validators_hash} \nNumber of txs: ${res.data.block.data.txs.length}\nTime: ${res.data.block.header.time}`;
-      console.log(msg);
-      await discord.info(
-        "Missed block",
-        `https://ping.pub/terra-luna/blocks/${res.data.block.header.height}`,
-        msg
+      console.log(
+        `Current blocks for checking: ${res.data.block.header.height}`
       );
-      // }
+      if (res.data.block.last_commit.signatures[4].validator_address === "") {
+        msg = `Block Proposer: \nhttps://stake.id/?#/validator/${res.data.block.header.proposer_address} \nNumber of txs: ${res.data.block.data.txs.length}\nTime: ${res.data.block.header.time}`;
+        log = `${res.data.block.header.height} | ${res.data.block.header.proposer_address} | ${res.data.block.data.txs.length} | ${res.data.block.header.time}`;
+        console.log(log);
+        await discord.info(
+          `Missed block: ${res.data.block.header.height}`,
+          `https://ping.pub/terra-luna/blocks/${res.data.block.header.height}`,
+          msg
+        );
+      }
       blockHeight++;
     });
-  setTimeout(checkBlockInfo, 7000);
+  setTimeout(checkBlockInfo, 5000);
 };
 
-console.log("BLOCK | VALIDATOR | DATE | NUMBER OF TX");
-checkBlockInfo();
+latestBlock().then(() => {
+  checkBlockInfo();
+});
